fc9da696c35ca58fd3720da20c2587e44f125e33
Nmuckler <77216931+Nmuckler@users.noreply.github.com>
2023-08-27 16:43:38 -0400

    Merged health injto CPP

--- Source/Swash/Swash.Build.cs
+++ Source/Swash/Swash.Build.cs
@@ -4,10 +4,14 @@ using UnrealBuildTool;
 
 public class Swash : ModuleRules
 {
-	public Swash(ReadOnlyTargetRules Target) : base(Target)
-	{
-		PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
+    public Swash(ReadOnlyTargetRules Target) : base(Target)
+    {
+        PCHUsage = PCHUsageMode.UseExplicitOrSharedPCHs;
 
-		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
-	}
+        //Core Dependencies
+        PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "HeadMountedDisplay", "EnhancedInput" });
+
+        //Gameplay Ability System Dependencies
+        PublicDependencyModuleNames.AddRange(new string[] { "GameplayAbilities", "GameplayTags", "GameplayTasks" });
+    }
 }
